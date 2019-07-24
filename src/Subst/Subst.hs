50e9edd8b68d07af069b6c031500f55a0ad47b9d
Yuya Kono <takohati0821@gmail.com>
2019-07-24 14:58:52 +0900

    src/Substを作成

---
+++ src/Subst/Subst.hs
@@ -0,0 +1,29 @@
+module Subst.Subst where
+
+import qualified Data.Map   as Map
+import qualified Data.Set   as Set
+import           Subst.Type
+
+type Subst = Map.Map TyVar Type
+
+compose :: Subst -> Subst -> Subst
+s1 `compose` s2 = fmap (apply s1) s2 `Map.union` s1
+
+class Substitutable a where
+  apply :: Subst -> a -> a
+  ftv :: a -> Set.Set TyVar
+
+instance Substitutable Type where
+  apply s t@(TyMeta a) = Map.findWithDefault t a s
+  apply s (TyApp c ts) = TyApp c $ apply s ts
+  ftv (TyMeta a)   = Set.singleton a
+  ftv (TyApp _ ts) = ftv ts
+
+instance Substitutable Scheme where
+  apply s (Forall as t) = Forall as $ apply s' t
+    where s' = foldr Map.delete s as
+  ftv (Forall as t) = ftv t `Set.difference` Set.fromList as
+
+instance Substitutable a => Substitutable [a] where
+  apply = map . apply
+  ftv = foldr (Set.union . ftv) Set.empty
---
+++ src/Subst/Type.hs
@@ -0,0 +1,13 @@
+module Subst.Type where
+
+type TyVar = Int
+
+data Type = TyMeta TyVar
+          | TyApp TyCon [Type]
+  deriving (Eq, Show)
+
+data TyCon = ArrowC | IntC | BoolC
+  deriving (Eq, Show)
+
+data Scheme = Forall [TyVar] Type
+  deriving (Eq, Show)
