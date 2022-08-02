0e879dd6e1aef121b5d4d30cd96805f28419454a
Yuya Kono <takohati0821@gmail.com>
2022-08-02 23:28:09 +0900

    Add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,40 @@
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+import qualified Constraint.Infer as Constraint
+import Parse (parseExp)
+import qualified Ref.Infer as Ref
+import qualified Subst.Infer as Subst
+import Test.Hspec
+import Data.Either (isRight, isLeft)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  checkAllTyping "fun x -> x"
+  checkAllTyping "(fun x -> x) 1"
+  checkAllInvalid "fun x -> x x"
+
+checkAllTyping :: String -> SpecWith ()
+checkAllTyping src = describe ("valid: " <> src) $ do
+  let ast = parseExp "<test>" src
+  it "Parse" $ ast `shouldSatisfy` isRight
+  let Right ast' = ast
+  typ <- runIO $ Ref.runInfer $ Ref.infer ast'
+  it "Ref" $ typ `shouldSatisfy` isRight
+  runIO $ print typ
+  let typ = Subst.runInfer $ Subst.infer ast'
+  it "Subst" $ typ `shouldSatisfy` isRight
+  runIO $ print typ
+  let typ = Constraint.runInfer $ Constraint.infer ast'
+  it "Constraint" $ typ `shouldSatisfy` isRight
+  runIO $ print typ
+
+checkAllInvalid :: String -> SpecWith ()
+checkAllInvalid src = describe ("invalid: " <> src) $ do
+  let ast = parseExp "<test>" src
+  it "Parse" $ ast `shouldSatisfy` isRight
+  let Right ast' = ast
+  typ <- runIO $ Ref.runInfer $ Ref.infer ast'
+  it "Ref" $ typ `shouldSatisfy` isLeft
+  let typ = Subst.runInfer $ Subst.infer ast'
+  it "Subst" $ typ `shouldSatisfy` isLeft
+  let typ = Constraint.runInfer $ Constraint.infer ast'
+  it "Constraint" $ typ `shouldSatisfy` isLeft
